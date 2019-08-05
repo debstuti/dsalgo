eb6c456541c6e5e61ca74844d90d3d9b72e7e998
debstuti <ddebstuti@gmail.com>
2019-08-05 23:00:05 +0530

    Update TimeMapDemo.java

--- TimeMapDemo.java
+++ TimeMapDemo.java
@@ -1,3 +1,20 @@
+/**
+https://leetcode.com/problems/time-based-key-value-store/
+
+Create a timebased key-value store class TimeMap, that supports two operations.
+
+1. set(string key, string value, int timestamp)
+
+    Stores the key and value, along with the given timestamp.
+
+2. get(string key, int timestamp)
+
+    Returns a value such that set(key, value, timestamp_prev) was called previously, with timestamp_prev <= timestamp.
+    If there are multiple such values, it returns the one with the largest timestamp_prev.
+    If there are no values, it returns the empty string ("").
+
+**/
+
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
