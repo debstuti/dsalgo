f9478696a35b290e8ee63b890a3bf441bd77c75e
debstuti <ddebstuti@gmail.com>
2020-04-01 14:26:48 +0530

    Leetcode 337

--- NestedList.java
+++ NestedList.java
@@ -1,4 +1,5 @@
 /*
+Leetcode 337
 Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
 
 Each element is either an integer, or a list -- whose elements may also be integers or other lists.
